d473a3ff1aaf3b10f5a5a7a1550e072eaa5bf337
Temuulen Batbileg <23LP8360@G304-CC0225.local>
2024-06-05 17:38:48 +0800

    sd

---
+++ app.js
@@ -0,0 +1,9 @@
+// async function fetchData() {
+//   const response = await fetch("http://localhost:4000/", {
+//     method: "POST",
+//     headers: { "Content-type": "application/json" },
+//     body: JSON.stringify({ query: "query{ognoo}" }),
+//   });
+//   const data = await response.json();
+//   console.log(data);
+// }
